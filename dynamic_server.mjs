24f0317f5beca380adfa010dbf1575c20c2d7e7b
line7313 <line7313@stthomas.edu>
2023-11-02 14:43:09 -0500

    Revert "Dynamic Buttons"
    This reverts commit aba01d97d1a3da645b673b7d62a623f690caad0b, reversing
    changes made to 20ece10b912c41ed31426a678cc5121af3a18f0b.

--- dynamic_server.mjs
+++ dynamic_server.mjs
@@ -1,138 +1,129 @@
-import * as fs from "node:fs";
-import * as path from "node:path";
-import * as url from "node:url";
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import * as url from 'node:url';
 
-import { default as express } from "express";
-import { default as sqlite3 } from "sqlite3";
+import { default as express } from 'express';
+import { default as sqlite3 } from 'sqlite3';
 
 const port = 8080;
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-const root = path.join(__dirname, "public");
-const templatePath = path.join(__dirname, "templates");
+const root = path.join(__dirname, 'public');
+const templatePath = path.join(__dirname, 'templates');
 let renderedTemplate = "";
 let titleAbbr = "";
-let errorMessage = "";
-let historyArray = [];
-let currentIndex = 0;
-let buttons = "";
-
-
-
 
 let app = express();
 app.use(express.static(root));
 
-let title = "";
-const db = new sqlite3.Database(
-  path.join(__dirname, "nhl_data.sqlite3"),
-  sqlite3.OPEN_READONLY,
-  (err) => {
+let title = '';
+const db = new sqlite3.Database(path.join(__dirname, 'nhl_data.sqlite3'), sqlite3.OPEN_READONLY, (err) => {
     if (err) {
-      console.log("Error connecting to database");
-    } else {
-      console.log("Successfully connected to database");
+        console.log('Error connecting to database');
     }
-  }
-);
+    else {
+        console.log('Successfully connected to database');
+    }
+});
 
 let template = new Promise((resolve, reject) => {
-  fs.readFile(path.join(templatePath, "temp.html"), "utf-8", (err, data) => {
-    if (err) {
-      reject(err);
-    } else {
-      resolve(data);
-    }
-  });
+    fs.readFile(path.join(templatePath, 'temp.html'), 'utf-8', (err, data) => {
+        if (err) {
+            reject(err);
+        } else {
+            resolve(data);
+        }            
+    })
 });
 
 let homeTemplate = new Promise((resolve, reject) => {
-  fs.readFile(path.join(templatePath, "home.html"), "utf-8", (err, data) => {
-    if (err) {
-      reject(err);
-    } else {
-      resolve(data);
-    }
-  });
+    fs.readFile(path.join(templatePath, 'home.html'), 'utf-8', (err, data) => {
+        if (err) {
+            reject(err);
+        } else {
+            resolve(data);
+        }            
+    })
 });
 
 function queryDatabase(column, modifier, queryOverride = false) {
-  return new Promise((resolve, reject) => {
-    let query;
-    let selectionModifier =
-      "season, date, home_team, away_team, home_team_abbr, away_team_abbr, home_team_score, away_team_score, game_quality_rating, game_importance_rating FROM nhl_data WHERE ";
-    if (queryOverride) {
-      query = `SELECT ${selectionModifier} ${column} ${modifier} ORDER BY ${column} + 1 DESC;` // need to cast number so that 8 will not come before 79
-    } else {
-      query = `SELECT ${selectionModifier} home_team_abbr = '${column}' OR away_team_abbr = '${column}'ORDER BY date DESC;`;
-    }
+    return new Promise((resolve, reject) => {
+        let query;
+        let selectionModifier = "season, date, home_team, away_team, home_team_abbr, away_team_abbr, home_team_score, away_team_score, game_quality_rating, game_importance_rating FROM nhl_data WHERE ";
+        if (queryOverride) {
+            query = `SELECT ${selectionModifier} ${column} ${modifier};`
+        } else {
+            query = `SELECT ${selectionModifier} home_team_abbr = '${column}' OR away_team_abbr = '${column}' ORDER BY date DESC;`
+        }
 
-    db.all(query, (err, rows) => {
-      if (err || rows.length == 0) {
-        reject();
-      } else {
-        resolve(rows);
-      }
-    });
-  });
+        db.all(query, (err, rows) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(rows);                
+            }
+        })
+    })    
 }
 
 function abbreviationMapper(abbr, column) {
-  let queryModifier;
 
-  switch (abbr) {
-    case "verylow":
-      queryModifier = "< 20";
-      titleAbbr = "Very Low";
-      break;
+    let queryModifier;
 
-    case "low":
-      queryModifier = `>= 20 AND ${column} < 40`;
-      titleAbbr = "Low";
-      break;
+    switch (abbr) {
+        case "verylow":
+            queryModifier = "< 20;"
+            titleAbbr = "Very Low"
+            break;
 
-    case "medium":
-      queryModifier = `>= 40 AND ${column} < 60`;
-      titleAbbr = "Medium";
-      break;
+        case "low":
+            queryModifier = `>= 20 AND ${column} < 40;`
+            titleAbbr = "Low";
+            break;
 
-    case "high":
-      queryModifier = `>= 60 AND ${column} < 80`;
-      titleAbbr = "High";
-      break;
-    case "veryhigh":
-      queryModifier = ">= 80";
-      titleAbbr = "Very High";
-      break;
+        case "medium":
+            queryModifier = `>= 40 AND ${column} < 60;`
+            titleAbbr = "Medium";
+            break;
+            
+        case "high":
+            queryModifier = `>= 60 AND ${column} < 80;`
+            titleAbbr = "High";
+            break;
+        case "veryhigh":
+            queryModifier = ">= 80;"
+            titleAbbr = "Very High";
+            break;
+        
+        default:
+            queryModifier = ">0" //return all
+    };
 
-    default:
-      queryModifier = null; //return all
-  }
-
-  return queryModifier;
+    return queryModifier;
 }
 
+
 function createTableRow(entrys) {
-  let tableEntrys = "<tr>";
+    let tableEntrys = '<tr>';
 
-  entrys.forEach((entry) => {
-    tableEntrys += "<td>" + entry + "</td>";
-  });
+    entrys.forEach((entry) => {
+        tableEntrys += '<td>' + entry + '</td>';
+    })
 
-  return tableEntrys + "</tr>";
+    return tableEntrys + '</tr>';
 }
 
 function createTableHead(columns) {
-  let tableHead = "<thead> <tr>";
+    let tableHead = '<thead> <tr>';
 
-  columns.forEach((column) => {
-    tableHead += "<th>" + column + "</th>";
-  });
+    columns.forEach((column) => {
+        tableHead += '<th>' + column + '</th>';
+    })
 
-  return tableHead + "</tr> </thead> <tbody>";
+    return tableHead + '</tr> </thead> <tbody>';
 }
 
 function renderTemplate(route, data, userInput) {
-<<<<<<< HEAD
 
     return new Promise((resolve, reject) => {
         let imageSource = '';
@@ -249,235 +240,75 @@ function renderTemplate(route, data, userInput) {
             }
             
             reject();
-=======
-  return new Promise((resolve, reject) => {
-    let imageSource = "";
-    let imageAlt = "";
-    template.then((template) => {
-      if (route == "team") {
-        //This is the route we specify when calling the renderTemplate function
-        let teamName = mapName(userInput, data);
-        title = `Game Data for ${teamName}`; //The title above the data
-        let table = ""; //Table structure is as follows, HEAD, ROW, ROW, ROW, etc
-        table += createTableHead([
-          "Date",
-          "Home Team",
-          "Away Team",
-          "Home Team Score",
-          "Away Team Score",
-        ]); // Create the labels for the columns we want for this route (will change depending on route)
-        table += "<tbody>"; // NEED TO PUT EACH ROW WITHIN THE BODY
-        data.forEach((game) => {
-          table += createTableRow([
-            game.date,
-            game.home_team,
-            game.away_team,
-            game.home_team_score,
-            game.away_team_score,
-          ]); // Populate a row for every game
->>>>>>> 97c1b0c5233cac7e2463df0e9cd27f67723eede2
         });
-        table += "</tbody>";
-        renderedTemplate = template.replace("##TITLE##", title); // title replacement
-        renderedTemplate = renderedTemplate.replace("##TABLE_DATA##", table); // table replacement
-        imageSource =
-          "https://cdn1.sportngin.com/attachments/photo/2721/4644/Mpls_Arena_large.jpg";
-        imageAlt = "A picture of a vintage hockey arena";
-        renderedTemplate = renderedTemplate.replace(
-          "##IMAGE_SRC##",
-          imageSource
-        );
-        renderedTemplate = renderedTemplate.replace("##IMAGE_ALT##", imageAlt);
-        //constructNextPrevious();
-        //renderedTemplate = renderedTemplate.replace("##NEXT_PREVIOUS##", buttons);
-        resolve(renderedTemplate);
-      } else if (route == "quality") {
-        let gameQuality = titleAbbr;
-        title = `Showing Data For ${gameQuality} Quality Games `;
-        let table = "";
-        table += createTableHead([
-          "Date",
-          "Home Team",
-          "Away Team",
-          "Game Quality Rating",
-        ]);
-
-        table += "<tbody>";
-        data.forEach((game) => {
-          table += createTableRow([
-            game.date,
-            game.home_team,
-            game.away_team,
-            game.game_quality_rating,
-          ]);
-        });
-        table += "</tbody>";
-        renderedTemplate = template.replace("##TITLE##", title);
-        imageSource =
-          "https://stevethedoc.files.wordpress.com/2020/02/1942483.jpg";
-        imageAlt = "A picture of a quality stamp";
-        renderedTemplate = renderedTemplate.replace(
-          "##IMAGE_SRC##",
-          imageSource
-        );
-        renderedTemplate = renderedTemplate.replace("##TABLE_DATA##", table); // table replacement
-        renderedTemplate = renderedTemplate.replace("##IMAGE_ALT##", imageAlt);
-        //constructNextPrevious();
-        //renderedTemplate = renderedTemplate.replace("##NEXT_PREVIOUS##", buttons);
-        resolve(renderedTemplate);
-      } else if (route == "importance") {
-        let teamName = titleAbbr;
-        title = `Showing Data For ${teamName} Importance Games `;
-        let table = ""; //Table structure is as follows, HEAD, ROW, ROW, ROW, etc
-        table += createTableHead([
-          "Date",
-          "Home Team",
-          "Away Team",
-          "Importance",
-        ]); // Create the labels for the columns we want for this route (will change depending on route)
-
-        table += "<tbody>"; // NEED TO PUT EACH ROW WITHIN THE BODY
-        data.forEach((game) => {
-          table += createTableRow([
-            game.date,
-            game.home_team,
-            game.away_team,
-            game.game_importance_rating,
-          ]); // Populate a row for every game
-        });
-
-        table += "</tbody>";
-        imageSource =
-          "https://www.picserver.org/assets/library/2020-10-13/originals/importance.jpg";
-        imageAlt = "A picture of a sign that says importance";
-        renderedTemplate = template.replace("##TITLE##", title); // title replacement
-        renderedTemplate = renderedTemplate.replace("##TABLE_DATA##", table); // table replacement
-        renderedTemplate = renderedTemplate.replace(
-          "##IMAGE_SRC##",
-          imageSource
-        );
-        renderedTemplate = renderedTemplate.replace("##IMAGE_ALT##", imageAlt);
-        //constructNextPrevious();
-        //renderedTemplate = renderedTemplate.replace("##NEXT_PREVIOUS##", buttons);
-        resolve(renderedTemplate);
-      }
-
-      reject();
-    });
-  });
+    });    
 }
-
-// function constructNextPrevious() {
-//   console.log(historyArray);
-//   buttons = "";
-//   currentIndex = currentIndex + 1;
-//   if(currentIndex > 0) {    
-//     buttons += `<div class="col-12 col-md-6 text-md-right" style="color: white;">
-//                 <a href="redirectButton?${historyArray[currentIndex-1]}&previous"><h4 class="text-center">Previous Page</h4></a>
-//                 </div>`
-//   } 
-//   else if (currentIndex + 8 < historyArray.length) {
-//     buttons += `<div class="col-12 col-md-6 text-md-right" style="color: white;">
-//                 <a href="${historyArray[currentIndex+1]}"><h4 class="text-center">Next Page</h4></a>
-//                 </div>`
-//   }
-// }
 
 function mapName(inputAbbr, data) {
-  if (inputAbbr == data[0].home_team_abbr) {
-    return data[0].home_team;
-  } else {
-    return data[0].away_team;
-  }
+
+    if (inputAbbr == data[0].home_team_abbr) {
+         return data[0].home_team;
+    } else {
+        return data[0].away_team;
+    }
 }
 
-<<<<<<< HEAD
 
 app.get('/', async (req, res) => {   
     homeTemplate.then((homeTemplate) => {
         res.status(200).type('html').send(homeTemplate);         
     }); 
-=======
-app.get("/", async (req, res) => {
-  historyArray.push('/');
-  currentIndex++;
-  homeTemplate.then((homeTemplate) => {
-    res.status(200).type("html").send(homeTemplate);
-  });
->>>>>>> 97c1b0c5233cac7e2463df0e9cd27f67723eede2
 });
 
-// app.get("/redirectButton", async (req, res) => {
-//   let buttonPressed = req.query[1];
-//   console.log(buttonPressed);
-// });
+app.get('/redirect', async (req, res) => {
+    let team = req.query.redirectTeam;
+    let quality = req.query.redirectQuality;
+    let importance = req.query.redirectImportance;
 
-
-app.get("/redirect", async (req, res) => {
-  let team = req.query.redirectTeam;
-  let quality = req.query.redirectQuality;
-  let importance = req.query.redirectImportance;
-
-  if (team) {
-    res.redirect(`/team/${team}`);
-  } else if (quality) {
-    res.redirect(`/quality/${quality}`);
-  } else {
-    res.redirect(`/importance/${importance}`);
-  }
+    if (team) {
+        res.redirect(`/team/${team}`);
+    } else if (quality) {
+        res.redirect(`/quality/${quality}`);
+    } else {
+        res.redirect(`/importance/${importance}`);
+    }
 });
 
-app.get("/team/:team", (req, res) => {
-  let teamAbbr = req.params.team.toUpperCase();
-  historyArray.push('/team/' + teamAbbr);
-  currentIndex++;
-  errorMessage =
-    "ERROR 404 NOT FOUND: Can not find game data for requested team abbreviation " +
-    teamAbbr;
- queryDatabase(teamAbbr)
-    .then((data) => {
-      renderTemplate("team", data, teamAbbr).then(() => {
-        res.status(200).type("html").send(renderedTemplate);
-      });
-    })
-    .catch(() => {
-      res.status(404).send(errorMessage);
+
+
+app.get('/team/:team', (req, res) => {
+    let teamAbbr = req.params.team.toUpperCase();
+    let data = queryDatabase(teamAbbr);
+    data.then((data) => {
+        renderTemplate("team", data, teamAbbr).then(() => {
+        res.status(200).type('html').send(renderedTemplate);         
+        });
     });
+    
 });
 
-app.get("/quality/:quality", async (req, res) => {
-  let column = "game_quality_rating";
-  let quality = req.params.quality.toLowerCase();
-  errorMessage ="ERROR 404 NOT FOUND: Can not find game data for " + quality + " quality";
-  historyArray.push('/quality/' + quality);
-  currentIndex++;
-  let queryModifier = abbreviationMapper(quality, column);
-  queryDatabase(column, queryModifier, true).then((rows) => {
-    renderTemplate("quality", rows, quality).then(() => {
-      res.status(200).type("html").send(renderedTemplate);
-    });
-  }).catch(() => {
-    res.status(404).send(errorMessage);
-  });
+app.get('/quality/:quality', async (req, res) => {   
+    let column = "game_quality_rating";
+    let quality = req.params.quality.toLowerCase();
+    let queryModifier = abbreviationMapper(quality, column);
+    let rows = queryDatabase(column, queryModifier, true);
+    rows.then((rows) => {
+        renderTemplate("quality", rows, quality).then(() => {
+            res.status(200).type('html').send(renderedTemplate);         
+        });
+    });    
 });
 
-app.get("/importance/:importance", async (req, res) => {
-  let column = "game_importance_rating";
-  let importance = req.params.importance.toLowerCase();
-  historyArray.push('/importance/' + importance);
-  currentIndex++;
-  errorMessage =
-    "ERROR 404 NOT FOUND: Can not find game data for " +
-    importance +
-    " importance";
-  let queryModifier = abbreviationMapper(importance, column);
-  let rows = queryDatabase(column, queryModifier, true).then((rows) => {
-    renderTemplate("importance", rows, importance).then(() => {
-      res.status(200).type("html").send(renderedTemplate);
-    });
-  }).catch(() => {
-    res.status(404).send(errorMessage);
-  });
+app.get('/importance/:importance', async (req, res) => {   
+    let column = "game_importance_rating";
+    let importance = req.params.importance.toLowerCase();
+    let queryModifier = abbreviationMapper(importance, column);
+    let rows = queryDatabase(column, queryModifier, true);
+    rows.then((rows) => {
+        renderTemplate("importance", rows, importance).then(() => {
+            res.status(200).type('html').send(renderedTemplate);         
+        });
+    }); 
 });
 
 
@@ -490,7 +321,7 @@ const menuItems = [
 ];
 
 app.listen(port, () => {
-  console.log("http://localhost:8080");
+    console.log('http://localhost:8080');
 });
 
 //test
